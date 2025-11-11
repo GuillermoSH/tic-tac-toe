3833acdd7c2e358a031ca93d8675e70fc2c8e34b
GuillermoSH <siciliahernandezguillermo@gmail.com>
2025-11-11 16:11:19 +0000

    :white_check_mark: Finalizada la v2 del tic tac toe

--- components/StatsPanel.tsx
+++ components/StatsPanel.tsx
@@ -3,7 +3,12 @@ import { useGameStats } from "@/hooks/useGameStats";
 import { Ionicons } from "@expo/vector-icons";
 import { Text, TouchableOpacity, View } from "react-native";
 
-export function StatsPanel() {
+type StatsPanelProps = {
+  winner: string | null;
+  isDraw: boolean;
+};
+
+export function StatsPanel({ winner, isDraw }: StatsPanelProps) {
   const { stats, resetStats } = useGameStats();
   const { themeType } = useTheme();
   const isDark = themeType === "dark";
@@ -19,16 +24,14 @@ export function StatsPanel() {
 
   return (
     <View
-      className={`p-6 rounded-2xl border shadow-lg ${
-        isDark
+      className={`p-6 rounded-2xl border shadow-lg ${isDark
           ? "bg-neutral-900 border-neutral-700"
           : "bg-white border-gray-200"
-      }`}
+        }`}
     >
       <Text
-        className={`text-2xl font-bold mb-6 text-center ${
-          isDark ? "text-neutral-100" : "text-gray-800"
-        }`}
+        className={`text-2xl font-bold mb-6 text-center ${isDark ? "text-neutral-100" : "text-gray-800"
+          }`}
       >
         Estadísticas de jugadores
       </Text>
@@ -38,23 +41,20 @@ export function StatsPanel() {
         {/* Jugador X */}
         <View className="flex-1 items-center border-r border-neutral-500 pr-3">
           <Text
-            className={`text-xl font-bold mt-2 ${
-              isDark ? "text-indigo-400" : "text-blue-500"
-            }`}
+            className={`text-xl font-bold mt-2 ${isDark ? "text-indigo-400" : "text-blue-500"
+              }`}
           >
             Jugador X
           </Text>
           <Text
-            className={`text-4xl font-extrabold ${
-              isDark ? "text-indigo-300" : "text-blue-600"
-            }`}
+            className={`text-4xl font-extrabold ${isDark ? "text-indigo-300" : "text-blue-600"
+              }`}
           >
             {winsX}
           </Text>
           <Text
-            className={`text-sm ${
-              isDark ? "text-neutral-400" : "text-gray-500"
-            }`}
+            className={`text-sm ${isDark ? "text-neutral-400" : "text-gray-500"
+              }`}
           >
             {winRateX.toFixed(1)}% victorias
           </Text>
@@ -63,23 +63,20 @@ export function StatsPanel() {
         {/* Jugador O */}
         <View className="flex-1 items-center pl-3">
           <Text
-            className={`text-xl font-bold mt-2 ${
-              isDark ? "text-amber-400" : "text-amber-500"
-            }`}
+            className={`text-xl font-bold mt-2 ${isDark ? "text-amber-400" : "text-amber-500"
+              }`}
           >
             Jugador O
           </Text>
           <Text
-            className={`text-4xl font-extrabold ${
-              isDark ? "text-amber-300" : "text-amber-600"
-            }`}
+            className={`text-4xl font-extrabold ${isDark ? "text-amber-300" : "text-amber-600"
+              }`}
           >
             {winsO}
           </Text>
           <Text
-            className={`text-sm ${
-              isDark ? "text-neutral-400" : "text-gray-500"
-            }`}
+            className={`text-sm ${isDark ? "text-neutral-400" : "text-gray-500"
+              }`}
           >
             {winRateO.toFixed(1)}% victorias
           </Text>
@@ -89,33 +86,30 @@ export function StatsPanel() {
       {/* Empates y partidas totales */}
       <View className="flex-row justify-between mb-4 px-2">
         <Text
-          className={`text-base font-medium ${
-            isDark ? "text-neutral-300" : "text-gray-600"
-          }`}
+          className={`text-base font-medium ${isDark ? "text-neutral-300" : "text-gray-600"
+            }`}
         >
           🤝 Empates: {draws}
         </Text>
         <Text
-          className={`text-base font-medium ${
-            isDark ? "text-neutral-300" : "text-gray-600"
-          }`}
+          className={`text-base font-medium ${isDark ? "text-neutral-300" : "text-gray-600"
+            }`}
         >
           🎮 Total: {total}
         </Text>
       </View>
 
       {/* Botón borrar */}
-      <TouchableOpacity
+      {(winner != null || isDraw) && (<TouchableOpacity
         onPress={resetStats}
-        className={`mt-2 py-3 rounded-xl flex-row justify-center items-center gap-2 active:scale-95 transition ${
-          isDark
+        className={`mt-2 py-3 rounded-xl flex-row justify-center items-center gap-2 active:scale-95 transition ${isDark
             ? "bg-red-600/80 border border-red-500"
             : "bg-red-500 border border-red-400"
-        }`}
+          }`}
       >
         <Ionicons name="trash-outline" size={18} color="#fff" />
         <Text className="text-white font-semibold">Borrar estadísticas</Text>
-      </TouchableOpacity>
+      </TouchableOpacity>)}
     </View>
   );
 }
